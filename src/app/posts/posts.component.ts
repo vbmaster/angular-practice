707ba543bcc9a835f8357f791c330d92eb692234
vbmaster <vbmaster@outlook.ph>
2021-02-15 19:15:42 +0800

    1 file modified

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders,  HttpParams,  HttpResponse} from '@angular/common/http';
 import{Observable, throwError} from 'rxjs';
-import 'rxjs/Rx';
 
 import{Post} from './post';
 @Component({
