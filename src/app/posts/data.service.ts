cbb3b7e154a0f0d545a8dd2f91688c25576e5464
Ronald Harry Preciados <vbmaster@outlook.ph>
2021-02-17 14:26:03 +0800

    Data composition

--- src/app/posts/data.service.ts
+++ src/app/posts/data.service.ts
@@ -1,27 +1,47 @@
+import { map, catchError, tap } from "rxjs/operators";
 import {
   HttpClient,
   HttpHeaders,
   HttpParams,
-  HttpResponse
+  HttpResponse,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Injectable } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { Post } from "./post";
-import 'rxjs/add/operator/catch';
-import "rxjs/add/observable/throw";
-import "rxjs/add/operator/map";
+// import 'rxjs/add/operator/catch';
+// import "rxjs/add/observable/throw";
+// import "rxjs/add/operator/map";
 import { CustomError } from "../common/custom-error";
 import { AppError } from "../common/app-error";
-
+import {pipe} from 'rxjs';
 @Injectable()
 export class DataService {
-  constructor( private http: HttpClient,  private ROOT_URL: string) {
-    
-  }
+
+  constructor(
+    private http: HttpClient,
+    @Inject("ROOT_URL") private ROOT_URL: string
+  ) {}
+
+  posts$ = this.http.get(this.ROOT_URL).pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+  
+
+  users$ = this.http.get("http://jsonplaceholder.typicode.com/users").pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  )
+
   getAll() {
+
+    
+
     let params = new HttpParams().set("userId", "1");
     let headers = new HttpHeaders().set("Authorization", "auth-token");
-    return this.http.get(this.ROOT_URL , { headers }).catch(this.handleError);
+    return this.http
+      .get(this.ROOT_URL, { headers })
+      .pipe(catchError(this.handleError)); //.catch(this.handleError);
   }
 
   create() {
@@ -29,16 +49,16 @@ export class DataService {
       id: null,
       userId: 23,
       title: "New Post",
-      body: "Hello world!"
+      body: "Hello world!",
     };
-    return this.http.post(this.ROOT_URL , data).catch(this.handleError);
+    return this.http.post(this.ROOT_URL, data); //.catch(this.handleError);
   }
 
-  private handleError(error: Response){
-      if(error.status===0){
-        return Observable.throw(new CustomError(error));
-      }else{
-        return Observable.throw( new AppError(error));
-      }
+  private handleError(error: Response) {
+    if (error.status === 0) {
+      return Observable.throw(new CustomError(error));
+    } else {
+      return Observable.throw(new AppError(error));
+    }
   }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,26 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {PostService} from './posts.service'
 import {AppError} from '../common/app-error';
 import{Post} from './post';
-import{Observable, throwError} from 'rxjs';
+import {PostWithUser} from './postwithuser';
+import{combineLatest, of, pipe} from 'rxjs';
 import { CustomError } from '../common/custom-error';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class PostsComponent {
   
   posts: any[];
   newPost: any;
+  errorMessage: any;
 
  constructor(private service: PostService){
  }
 
+ posts$ = this.service.posts$.pipe(
+   catchError( error=>{
+     this.errorMessage= error;
+     return of (null)
+   })
+ );
+
+ users$ = this.service.users$.pipe(
+   catchError((error)=>{
+     this.errorMessage=error;
+     return of(null);
+   })
+ )
+
+ postwithuser$ = combineLatest(this.posts$, this.users$).pipe(
+   map(([posts, users])=>
+   posts.map({
+     ...posts,
+     username: users.find(u=>u.id===posts.userId).name
+   } as PostWithUser[] )
+   )
+ );
+
  getPosts(){
   this.service.getAll().subscribe(posts => this.posts= (posts as Post[]));
+ //console.log('get post', this.postwithuser$);
+ 
  }
 
  createPost(){
